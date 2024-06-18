ddcdfaa1bacf51169f57d4d58dd6b50dc194d367
Alexander Lead <a.lead@yandex.ru>
2024-06-18 21:50:35 +0300

    feat(app): react install

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomePage from './views/HomePage'
+
+function App() {
+
+  return (
+    <>
+
+      <Routes>
+      <Route path='/' element={<HomePage />} >
+      </Route>
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/views/HomePage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+interface IHomePageProps {
+}
+
+const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
+  return (
+    <Container>
+        <Row>
+        <Col>
+        
+        </Col>
+        </Row>
+    </Container>
+  );
+};
+
+export default HomePage;
